ed95e522ca270b9920bdd41efd7413f0072d1657
SatyaManohar-Git <manoharsha2799@gmail.com>
2025-04-12 20:20:50 +0530

    Update main.py
    added head.
    Trial 1

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/", include_in_schema=False)
+@app.head("/", include_in_schema=False)
 async def root():
     return {"message" :"Hello World"}
