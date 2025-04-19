8e0635e06b34bc2b259dd26b62074a09408d9ec4
SatyaManohar-Git <manoharsha2799@gmail.com>
2025-04-19 17:08:53 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-
+from app.auth import app_auth as auth_app
 
 app = FastAPI()
+app.include_router(auth_app, prefix="/authentication")
 
-@app.get("/", include_in_schema=False)
+@app.get("/",include_in_schema=False)
 @app.head("/", include_in_schema=False)
-async def root():
-    return {"message" :"Hello World"}
+def root():
+    return "Home Page ......"
